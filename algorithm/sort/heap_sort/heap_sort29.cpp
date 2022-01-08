9319522ca491a4e8102f977e1aaf843345b4315c
huangjunhao2019 <huang.jun.hao@foxmail.com>
2022-01-08 19:41:16 +0800

    1.8

--- algorithm/sort/heap_sort/heap_sort29.cpp
+++ algorithm/sort/heap_sort/heap_sort29.cpp
@@ -4,16 +4,16 @@ using namespace std;
 void heapify(vector<int> &nums,int idx,int max){
     int left=idx*2+1;
     int right=idx*2+2;
-    int small=idx;
-    if(left<max && nums[left]<nums[small]){
-        small=left;
+    int largest=idx;
+    if(left<max && nums[left]>nums[largest]){
+        largest=left;
     }
-    if(right<max && nums[right]<nums[small]){
-        small=right;
+    if(right<max && nums[right]>nums[largest]){
+        largest=right;
     }
-    if(small!=idx){
-        swap(nums[small],nums[idx]);
-        heapify(nums,small,max);
+    if(largest!=idx){
+        swap(nums[idx],nums[largest]);
+        heapify(nums,largest,max);
     }
 }
 void buildHeap(vector<int> &nums){
@@ -23,7 +23,7 @@ void buildHeap(vector<int> &nums){
 }
 void heap_sort(vector<int> &nums){
     buildHeap(nums);
-    for(int i=nums.size()-1;i>=0;i--){
+    for(int i=nums.size()-1;i>=1;i--){
         swap(nums[i],nums[0]);
         heapify(nums,0,i);
     }
@@ -31,7 +31,7 @@ void heap_sort(vector<int> &nums){
 int main(){
     vector<int> nums;
     for(int i=0;i<10;i++){
-        nums.push_back(i);
+        nums.push_back(10-i);
     }
     heap_sort(nums);
     for(int i=0;i<nums.size();i++){
